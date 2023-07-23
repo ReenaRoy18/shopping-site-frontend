e0a0549a57a2f73e2de8418a67ce25e45c0898be
harshlakhara <harshlakhara007@gmail.com>
2023-07-23 17:29:53 +0530

    added login and admin component

---
+++ src/app/admin/login/login.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shopping-admin-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent {}
